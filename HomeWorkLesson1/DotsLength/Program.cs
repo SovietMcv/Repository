1aff305f88f19d7f7d5d8c8ec3a4ec6013672af5
Anton <polbupalkoi@gmail.com>
2020-06-02 00:04:59 +0300

    Домашнее задание к уроку 1

---
+++ HomeWorkLesson1/DotsLength/Program.cs
@@ -0,0 +1,43 @@
+﻿//Выполняет Федосенко Антон Александрович
+//а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2). Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+//б) * Выполните предыдущее задание, оформив вычисления расстояния между точками в виде метода;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MethodsLib;
+
+namespace DotsLength
+{
+    class Program
+    {
+        static double x1;
+        static double x2;
+        static double y1;
+        static double y2;
+        static double dlength;       
+        static double GetDotsLegth (double x1, double y1, double x2, double y2)
+        {
+            var res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return res;
+        }
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Добрый день! Сегодня мы попробуем посчитать расстояние между двумя координатами. Для этого вам потребуется последовательно ввести значение x,y для обоих координат");
+            Console.WriteLine("Введите значение координаты x1");
+            x1 = MyMethods.NumsCheck(Console.ReadLine());
+            Console.WriteLine("Введите значение координаты y1");
+            y1 = MyMethods.NumsCheck(Console.ReadLine());
+            Console.WriteLine("Введите значение координаты x2");
+            x2 = MyMethods.NumsCheck(Console.ReadLine());
+            Console.WriteLine("Введите значение координаты y2");
+            y2 = MyMethods.NumsCheck(Console.ReadLine());
+            dlength = GetDotsLegth(x1, y1, x2, y2);
+            Console.WriteLine("Расстояние между координатами равно " + $"{dlength:F}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWorkLesson1/MethodsLib/MyLibs.cs
@@ -0,0 +1,37 @@
+﻿//Выполняет Федосенко Антон Александрович
+//Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause).
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsLib
+{
+    public static class MyMethods
+    {
+        public static double NumsCheck(string text)
+        {
+            var res = 0.0;
+            if (Double.TryParse(text, out res) == false)
+            {
+                Console.WriteLine("Некорректное значение, повторите ввод пожалуйста.");
+                var newdata = Console.ReadLine();
+                res = Convert.ToDouble(NumsCheck(newdata));
+            }
+            else
+            {
+                res = Convert.ToDouble(text);
+            }
+            return res;
+        }
+        public static void SetTextPosition(string text,int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}

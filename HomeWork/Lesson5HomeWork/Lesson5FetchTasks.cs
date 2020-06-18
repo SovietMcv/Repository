e8c4c6a6ec0767da3396560f0e5f0b518d55a227
Anton <polbupalkoi@gmail.com>
2020-06-19 01:04:47 +0300

    Домашнее задание 5

---
+++ HomeWork/Lesson5HomeWork/Lesson5FetchTasks.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeWorkLesson2;
+using MethodsLib;
+
+namespace Lesson5HomeWork
+{
+    class Lesson5FetchTasks
+    {
+        static string[] TaskMap = new string[] { "NumPad1 = Авторизация с проверкой через регулярные выражения", "NumPad2 = Демонстрация собственного класса Message", "Numpad3 = Сравнение строк на перестановку","NumPad4 = Задача ЕГЭ","NumPad5 = Игра Верю - Не верю" ,"Escape = Выйти из программы" };
+        static ConsoleKey[] KeyValues = new ConsoleKey[] { ConsoleKey.NumPad1, ConsoleKey.NumPad2, ConsoleKey.NumPad3, ConsoleKey.NumPad4, ConsoleKey.NumPad5, ConsoleKey.Escape };
+        static ConsoleKeyInfo key;
+        static bool GoForward = false;
+
+        static void FetchTasks(ConsoleKey key)
+        {
+
+            switch (key)
+            {
+                case ConsoleKey.NumPad1:
+                    {
+                        Console.Clear();
+                        do
+                        {
+                            Console.WriteLine("Укажите какой способ авторизации вы хотите запустить. 1 - С регулярными выражениям, 0 - без регулярных выражений");
+                            double k = MyMethods.NumsCheckNoRestr(Console.ReadLine());
+                            switch (k)
+                            {
+                                case 1:
+                                    {
+                                        HomeWorkTasks.Authorization();
+                                        GoForward = true;
+                                    }
+                                    break;
+                                case 0:
+                                    {
+                                        HomeWorkTasks.AuthorizationWithOutRegEx();
+                                        GoForward = true;
+                                    }
+                                    break;
+                            }
+                        }
+                        while (!GoForward);
+                        GoForward = false;                                              
+                    }
+                    break;
+                case ConsoleKey.NumPad2:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Укажите путь к файлу");
+                        string path = Console.ReadLine();
+                        Message.GetText(path);
+                        Console.WriteLine($"Ваш текст: {Message.Text}");
+                        Console.WriteLine();
+                        Console.WriteLine($"Одно из самых длинных слов: {Message.LongestWord}");
+                        Console.WriteLine();
+                        Console.WriteLine("Укажите, что вы хотите сделать с текстом. 1 - Вывести только те слова которые содержат не более N букв. 2 - Удалить те слова, у которых на конце есть символ. 3 - сформировать StringBuilder из всех самых длинных слов ");
+                        switch(MyMethods.NumsCheckNoRestr(Console.ReadLine()))
+                        {
+                            case 1:
+                                {
+                                    do
+                                    {
+                                        try
+                                        {
+                                            Console.WriteLine("Укажите количество букв");
+                                            Console.WriteLine($"Текст из слов не больше указанной длины: {Message.BuildTheString(Convert.ToInt32(MyMethods.NumsCheckNoRestr(Console.ReadLine())))}");
+                                            GoForward = true;
+                                        }
+                                        catch
+                                        {
+
+                                        }
+                                    }
+                                    while (!GoForward);
+                                }
+                                break;
+                            case 2:
+                                {
+                                    do
+                                    {
+                                        Console.WriteLine("Укажите символ для отбора строк");
+                                        try
+                                        {
+                                            Message.DeleteWordThatEndsWithSpecificSymbol(Convert.ToChar(Console.ReadLine()));
+                                            Console.WriteLine($"Получившийся текст: {Message.Text}");                                           
+                                            GoForward = true;
+                                        }
+                                        catch { Console.WriteLine("Введён некорректный символ"); }
+                                    }
+                                    while (!GoForward);
+                                    GoForward = false;        
+                                }
+                                break;
+                            case 3:
+                                {
+                                    Console.WriteLine($"Вывожу строку слов максимальной длины: {Message.BuildTheString(Message.StringsList)}");
+                                    GoForward = true;
+                                }
+                                break;
+                        }
+                    }
+                    break;
+                case ConsoleKey.NumPad3:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Укажите первое слово");
+                        Reallocation.firstline = Console.ReadLine();
+                        Console.WriteLine("Укажите второе слово");
+                        Reallocation.secondline = Console.ReadLine();
+                        if (Reallocation.ReallocationCheckDictionary()) Console.WriteLine("Второе слово является перестановкой первого"); else Console.WriteLine("Второе слово не является перестановкой первого");
+                    }
+                    break;
+                case ConsoleKey.NumPad4:
+                    {                       
+                        do
+                        {
+                            try
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Укажите путь к файлу");
+                                UGEtask.ShowBestAndWorstStudents(Console.ReadLine());
+                                //Можно было упростить, но я захотел в явном виде их разделить на два списка
+                                Console.WriteLine("Лучшие студенты");
+                                foreach(var el in UGEtask.BestStudents)
+                                { Console.WriteLine($"Фамилия и имя: {el.Key}, средний бал: {el.Value:F}"); }
+                                Console.WriteLine("Худшие студенты");
+                                foreach (var el in UGEtask.WorstStudents)
+                                { Console.WriteLine($"Фамилия и имя: {el.Key}, средний бал: {el.Value:F}"); }
+                                GoForward = true;
+                            }
+                            catch (Exception ex)
+                            {
+                            Console.WriteLine(ex.Message);
+                            }
+                        }
+                            while (!GoForward);
+                        }
+                    break;
+                case ConsoleKey.NumPad5:
+                    {
+                        do
+                        {
+                            try
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Укажите путь к файлу");
+                                BelieveOrNotBelieve.RiddlesProcessing(Console.ReadLine());
+                                int points = 0;
+                                foreach(var el in BelieveOrNotBelieve.CurrentRiddles)
+                                {
+                                    Console.WriteLine($"Ваша загадка: {el.Key}");
+                                    double answer;
+                                    //Немного упростил, чтобы не парсить строку и не приводить её к нужному через ToLower()                                   
+                                    do
+                                    {
+                                        Console.WriteLine("Введите 1 - если Да, 0 - если Нет");
+                                        answer = MyMethods.NumsCheckNoRestr(Console.ReadLine());
+                                    }
+                                    while (answer > 1);
+                                    if(answer == el.Value)
+                                    {
+                                        points++;
+                                        Console.WriteLine($"Ответ верный, у вас теперь {points} очк(о,а,ов)");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"Ответ неверный");
+                                        Console.WriteLine();
+                                    }
+                                }
+                                Console.WriteLine($"Итоговый счёт {points} очк(о,а,ов)");
+                                GoForward = true;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+                        while (!GoForward);
+                    }
+                    break;
+            }
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Добрый день! Давайте выберем задачу для выполнения.Для вывода списка возможных задач нажмите любую клавишу.");
+                Console.ReadLine();
+                foreach (var x in TaskMap)
+                {
+                    Console.WriteLine(x);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Чтобы выбрать задачу нажмите указанную клавишу в списке");
+                key = Console.ReadKey();
+                Console.WriteLine();
+                foreach (var n in KeyValues)
+                {
+                    if (key.Key == n && key.Key != ConsoleKey.Escape)
+                    {
+                        FetchTasks(key.Key);
+                        break;
+                    }
+                    else if (key.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    }
+                }
+            }
+            while (key.Key != ConsoleKey.Escape);
+            Console.WriteLine("Всего доброго!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork/Lesson5HomeWork/UGEtask.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeWorkLesson2;
+using MethodsLib;
+
+namespace Lesson5HomeWork
+{
+    class UGEtask
+    {
+        static Dictionary<string, double> StudensJournal { get; set; } = new Dictionary<string, double>();
+        static List<string> StingList = new List<string>();
+        public static Dictionary<string, double> BestStudents { get; private set; } = new Dictionary<string, double>();
+        public static Dictionary<string, double> WorstStudents { get; private set; } = new Dictionary<string, double>();
+        public static double MaxRating { get; private set; } = 0;
+        public static double MinRating { get; private set; } 
+        public static void ShowBestAndWorstStudents(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+            if (StingList != null) StingList.Clear();
+            if (StudensJournal != null) StudensJournal.Clear();
+            if (BestStudents != null) BestStudents.Clear();
+            if (WorstStudents != null) WorstStudents.Clear();
+            string s = "";
+            while (s != null) {s = sr.ReadLine(); if (s == null) break; StingList.Add(s);}
+            StingList.RemoveAt(0);
+            foreach (string c in StingList)
+            {
+                string[] arr =  c.Split(' ');
+                char res;
+                //Сделано так умышленно, потому что предполагается строгий шаблон, но всё на всякий случай вставил проверку и если что в Exception вывалится
+                string FI = (!Char.TryParse(arr[0], out res) && !Char.TryParse(arr[1], out res)) ? arr[0] + " " + arr[1] : "";
+                double AvarageRating = 0;
+                for (int i = 2; i< arr.Length;i++ ) { AvarageRating += Convert.ToDouble(arr[i]);}
+                AvarageRating = AvarageRating / (arr.Length - 2);
+                StudensJournal.Add(FI, AvarageRating);
+            }
+            //Ещё не до конца понимаю, как это работает, но очень удобно
+            var SortedList = StudensJournal.OrderByDescending(x => x.Value);
+            for (int i=0;i<3;i++)
+            {
+                BestStudents.Add(SortedList.ElementAt(i).Key, SortedList.ElementAt(i).Value);
+                WorstStudents.Add(SortedList.ElementAt(SortedList.Count() - i - 1).Key, SortedList.ElementAt(SortedList.Count() - i - 1).Value);
+            }
+            foreach (var el in SortedList) 
+            { 
+                for (int i = 0; i < WorstStudents.Count();i++)
+                {
+                    if(el.Value == WorstStudents.ElementAt(i).Value && el.Key != WorstStudents.ElementAt(i).Key && !WorstStudents.Contains(el))
+                    {
+                        WorstStudents.Add(el.Key, el.Value);
+                    }
+                }
+            }
+        }
+    }
+}

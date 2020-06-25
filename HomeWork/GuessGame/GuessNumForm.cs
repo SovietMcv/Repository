b2ab2e77d060ff7481d4d960c10a64e8539bda3f
Anton <polbupalkoi@gmail.com>
2020-06-26 00:50:09 +0300

    Домашняя работа 7

---
+++ HomeWork/GuessGame/GuessNumForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessGame
+{
+    public partial class GuessNumForm : Form
+    {
+        GuessGame guessGame;
+        public GuessNumForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (guessGame == null) { MessageBox.Show("Игра не начата."); return; }
+            guessGame.CheckNum(AnswerText.Text);
+            RefreshData();
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            guessGame = new GuessGame();
+            Start.Enabled = false;
+        }
+        private void RefreshData()
+        {
+            if (guessGame.WinParametr == 1)
+            {
+                history.Text = null;
+                ContextText.Text = "Загадано число от 1 до 100.Ваша задача угадать его.Введите значение.";
+                guessGame = null;
+                Start.Enabled = true;
+            }
+            else { history.Text = guessGame.history; }       
+        }
+    }
+}
---
+++ HomeWork/WonFormsGames/UdvoitelForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WonFormsGames
+{
+    public partial class UdvoitelForm : Form
+    {
+        Udvoitel u;
+        public UdvoitelForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            u = new Udvoitel();
+            RefreshData();
+        }
+
+        private void SumButtonClick(object sender, EventArgs e)
+        {
+            if (u == null) Udvoitel.ShowMessage(1);
+            else {u.Plus(); RefreshData(); }
+
+        }
+        private void RefreshData()
+        {
+            TextBox.Text = $"Значение, которого нужно достигнуть: {u.Finish}, Текущее значение: {u.Current}, Правильное количество шагов: {u.Steps}, Текущее количество шагов: {u.Count}";
+            if (u.Current == u.Finish)
+            {
+                Udvoitel.ShowMessage(3);
+                TextBox.Text = $"Значение, которого нужно достигнуть: 0, Текущее значение: 0, Правильное количество шагов: 0, Текущее количество шагов: 0";
+                u = null;
+            }                    
+        }
+
+        private void Multi_Click(object sender, EventArgs e)
+        {
+            if (u == null) Udvoitel.ShowMessage(1);
+            else { u.Multi(); RefreshData(); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (u == null) Udvoitel.ShowMessage(1);
+            else { u.Back(); RefreshData(); }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (u == null) Udvoitel.ShowMessage(1);
+            else { u.Reset(); RefreshData(); }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Udvoitel.ShowMessage(2);
+            Close();
+        }
+    }
+}
